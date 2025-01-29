7f799be64c3223ca32af86410707cdc406a4b2b0
Szilasi Martin <szilasimartin05@gmail.com>
2025-01-29 09:04:16 +0100

    mappak
    frontend backend kulonmappaban

---
+++ frontend/.vite/deps/primevue_config.js
@@ -0,0 +1,17 @@
+import {
+  PrimeVue,
+  clearConfig,
+  defaultOptions,
+  setup,
+  setupConfig,
+  usePrimeVue
+} from "./chunk-VXPQB4OT.js";
+import "./chunk-IJV5NOMV.js";
+export {
+  clearConfig,
+  PrimeVue as default,
+  defaultOptions,
+  setup,
+  setupConfig,
+  usePrimeVue
+};
