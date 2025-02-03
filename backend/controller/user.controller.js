89401027cf3df1a007e1621b76e0fdff71ee82ca
Vida Nolen <vidranolen19@gmail.com>
2025-02-03 08:35:08 +0100

    user contr serv ind
    user contr serv ind

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { listAllUsers, addUser, deleteUser, updateUser, login, logout } from '../services/user.service.js';
+import { genAPIKey } from '../services/auth.service.js'
+
+const router = express.Router();
+
+router.get("/getAll", async (req, res) => {
+    const data = await listAllUsers();
+    res.status(200).json(data);
+})
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body)
+    const data = await login(username, password);
+
+    if(data) {
+        const apiKey = await genAPIKey(data)
+        console.log("user", apiKey)
+        res.cookie('apiKey', apiKey, { maxAge: 3_600_000, httpOnly: true })
+        res.status(200).json({ message: data })
+    } else {
+        res.status(401).json({ message: "Bad Request" })
+    }
+})
+
+
+router.delete("/logout", async (req, res) => {
+    const user_id = Number(req.query.id);
+
+    await logout(user_id);
+
+    res.clearCookie("apiKey")
+    res.status(200).json({ message: "Successfully logged out" })
+})
+
+router.post("/add", async (req, res) => {
+    const { username, password, email, birth_date } = req.body;
+
+    await addUser(username, password, email, birth_date).then(() => {
+        res.status(201).json({
+            message: "Data successfully inserted"
+        })
+    }).catch(() => {
+        res.status(409).json({
+            message: "Duplicated data"
+        })
+    })
+})
+
+router.delete("/delete", async (req, res) => {
+    const id = Number(req.query.id);
+
+    await deleteUser(id);
+
+    res.status(204).json({
+        message: "Data successfully deleted"
+    })
+})
+
+router.put("/update", async (req, res) =>{
+    const id = Number(req.query.id);
+    const { password, email } = req.body;
+
+    await updateUser(id, password, email);
+
+    res.status(200).json({
+        message: "Data successfully updated"
+    })
+})
+
+export { router };
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+/* import express from "express";
 
 
 const app = express()
@@ -10,4 +10,41 @@ app.use(express.json());
 
 app.listen(port, () => {
     console.log(`App started at http://localhost:${port}`)
-});
+}); */
+
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { router as userRouter }  from './controller/user.controller.js';
+import { router as chatRouter }  from './controller/chat.controller.js';
+import { router as authRouter }  from './controller/auth.controller.js';
+import { listAllUsers } from './services/user.service.js'
+import { listAllMessages } from './services/chat.service.js'
+import { verifyApiKey } from './middleware/auth.middleware.js'
+
+const app = express()
+const port = 3300
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.set("view engine", "ejs")
+app.use("/user", userRouter)
+app.use("/chat", verifyApiKey, chatRouter)
+app.use("/auth", authRouter)
+
+app.get("/", async (req, res) => {
+    const userData = await listAllUsers();
+
+    res.render("index", {
+        users: userData
+    })
+})
+
+app.get("/login", async (req, res) => {
+    res.render("login")
+})
+
+app.listen(port, () => {
+    console.log(`App started at http://localhost:${port}`)
+})
