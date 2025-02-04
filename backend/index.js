1796cce8f1fc1a2f54dc06839581bdbecd610e2d
Szilasi Martin <szilasimartin05@gmail.com>
2025-02-04 09:13:36 +0100

    cors
    cors

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,23 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import { router as userRouter }  from './controller/user.controller.js';
 import { listAllUsers } from './services/user.service.js'
+import cors from "cors";
 
 const app = express()
 const port = 3300
 
+const corsOptions = {
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+  
+
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors(corsOptions));
 app.set("view engine", "ejs")
 app.use("/user", userRouter)
 
