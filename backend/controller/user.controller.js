40e9404c60a3983e69f51b5ec3b9a6896b0f6873
Szilasi Martin <szilasimartin05@gmail.com>
2025-02-04 11:29:07 +0100

    login,register,logout

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -8,26 +8,69 @@ router.get("/getAll", async (req, res) => {
     res.status(200).json(data);
 })
 
+router.post("/register", async (req, res) => {
+    const { username, email, password, nev, om, groupsNeve } = req.body;
+    try {
+      const user = await register(username, email, password, nev, om, groupsNeve);
+      res.status(201).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: error.message });
+    }
+  });
+
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    console.log(req.body)
-    const data = await login(username, password);
-    
-        res.status(200).json({ message: data })
-    {
-        res.status(401).json({ message: "Bad Request" })
+  
+    try {
+      const user = await login(username, password);
+  
+      if (!user.access_token || !user.refresh_token) {
+        res.status(401).json({ message: "Hibás felhasználó név vagy jelszó!" });
+        return;
+      }
+      res.cookie("access_token", user.access_token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: true,
+        httpOnly: false,
+        domain: "localhost:5173",
+        path: "/",
+      });
+      res.cookie("refresh_token", user.refresh_token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: false,
+        sameSite: "none",
+        secure: true,
+        domain: "localhost:5173",
+        path: "/",
+      });
+  
+      req.session.user_id = user.user_id;
+  
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
-})
+  });
+  
 
 
-router.delete("/logout", async (req, res) => {
-    const user_id = Number(req.query.id);
-
-    await logout(user_id);
-
-    res.clearCookie("apiKey")
-    res.status(200).json({ message: "Successfully logged out" })
-})
+  router.post("/logout", (req, res) => {
+    res.clearCookie("access_token", {
+      domain: "pollak.info",
+      path: "/",
+    });
+    res.clearCookie("refresh_token", {
+      domain: "pollak.info",
+      path: "/",
+    });
+    res.clearCookie("sid", {
+      domain: "pollak.info",
+      path: "/",
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+  });
 
 router.post("/add", async (req, res) => {
     const { username, password, email, admin, full_name } = req.body;
