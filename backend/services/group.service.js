4e01946948ed3fa20ab61f1080af223be7c5b3fd
molnaradam0302 <molnaradam0302@gmail.com>
2025-02-12 10:19:09 +0100

    admin felulet

--- backend/services/group.service.js
+++ backend/services/group.service.js
@@ -1,47 +1,61 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function listAllGroup() {
-    const data = await prisma.groups.findMany({
-        include: {
-            users: true
-        }
-    });
+  const data = await prisma.groups.findMany({
+    include: {
+      users: true,
+    },
+  });
 
-    return data;
+  return data;
 }
 
 export async function addGroup(name, write, read, update, del) {
-    await prisma.groups.create({
-        data: {
-            name: name,
-            write: write,
-            read: read,
-            update: update,
-            del: del
-        }
-    }).catch((err) => {
-        console.error(err)
-        throw new Error();
+  console.log(write, read, update, del);
+  await prisma.groups
+    .create({
+      data: {
+        name: name,
+        write: write,
+        read: read,
+        update: update,
+        del: del,
+      },
     })
+    .catch((err) => {
+      console.error(err);
+      throw new Error();
+    });
 }
 
 export async function deleteGroup(name) {
-    await prisma.groups.delete({
-        where: {
-            name: name
-        }
-    })
+  await prisma.groups.delete({
+    where: {
+      name: name,
+    },
+  });
 }
 
-export async function updateGroup(name, newname) {
-    await prisma.groups.update({
-        where: {
-            name: name
-        },
-        data: {
-            name: newname
-        }
-    })
+export async function updateGroup(
+  name,
+  newname,
+  writeUjJog,
+  readUjJog,
+  updateUjJog,
+  delUjJog
+) {
+  await prisma.groups.update({
+    where: {
+      name: name,
+    },
+    data: {
+      name: newname,
+      write: writeUjJog,
+      read: readUjJog,
+      update: updateUjJog,
+      del: delUjJog,
+    },
+  });
 }
